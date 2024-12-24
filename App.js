8c04f1b97b420365c237dfe9ada326aeb707feaf
César Guillermo Nevárez <memonevarez@hotmail.com>
2024-12-24 18:59:54 +0000

    MenuWithFlatList

--- App.js
+++ App.js
@@ -3,13 +3,16 @@ import { StyleSheet, Text, View } from "react-native";
 import LittleLemonHeader from "./components/LittleLemonHeader";
 import LittleLemonFooter from "./components/LittleLemonFooter";
 import WelcomeScreen from "./components/WelcomeScreen";
+import MenuItems from "./components/MenuItems";
+import MenuItemsFlatList from "./components/MenuItemsFlatList";
 
 export default function App() {
   return (
     <>
-      <View style={{ flex: 1, backgroundColor: "#495E57" }}>
+      <View style={styles.container}>
         <LittleLemonHeader />
         <WelcomeScreen />
+        <MenuItemsFlatList />
       </View>
       <View style={{ backgroundColor: "#495E57" }}>
         <LittleLemonFooter />
@@ -17,3 +20,10 @@ export default function App() {
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#495E57",
+  },
+});
